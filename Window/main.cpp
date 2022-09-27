3b18c344047eb57efa0f800a102edd6b5f5812ab
SuwhanBaek <SuwhanBaek@BCML-MAC1ui-MacBookAir.local>
2022-09-27 14:57:52 +0900

    Major Docu update

--- Window/main.cpp
+++ Window/main.cpp
@@ -1,13 +1,1 @@
-#include "Manager.h"
-
-int main(int argc, char* argv[])
-{
-    const char* commandFilepath = "command.txt";
-    if (argc > 1)
-        commandFilepath = argv[1];
-
-    Manager m;
-    m.Run(commandFilepath);
-
-    return 0;
-}
+// Implement code on Manager.c file (recommandation)
